654b9bbf03c3bd748fffdd2006df1b2063b937d8
Facundo Arballo <farballo@fi.uba.ar>
2024-07-15 14:47:45 -0300

    Agrego elementos y acomodo código

--- scripts.js
+++ scripts.js
@@ -3,7 +3,21 @@ document.getElementById('contacto').addEventListener('submit', function(event) {
     alert('Formulario enviado');
 });
 
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 3){
+        counter = 1;
+    }
+}, 5000);
+
+
 document.addEventListener("DOMContentLoaded", function() {
+    initializeImageObserver();
+});
+
+function initializeImageObserver() {
     const images = document.querySelectorAll(".products img");
     const observerOptions = {
         root: null,
@@ -23,39 +37,4 @@ document.addEventListener("DOMContentLoaded", function() {
     images.forEach(img => {
         observer.observe(img);
     });
-});
-setInterval(showNextImage, 5000);
-
-
-var autoplayInterval = 3000;
-var autoplayTimer = null;
-var autoplay = true;
-var newIndex = 1;
-
-if (autoplay) {
-        autoplayTimer = setInterval(function() {
-        newIndex++;
-        navigateSlider();
-    }, autoplayInterval);
-}
-
-function resetSlider() { 
-    clearInterval(autoplayTimer);
-}
-
-function navigateSlider() { 
-    const slide1 = document.getElementById('slide1');
-    const slide2 = document.getElementById('slide2');
-    const slide3 = document.getElementById('slide3');
-    const slide4 = document.getElementById('slide4');
-    if (newIndex == 1) {
-        slide1.checked = true;
-    } else if (newIndex == 2) {
-        slide2.checked = true;
-    } else if (newIndex == 3) {
-        slide3.checked = true;
-    } else if (newIndex == 4) {
-        slide4.checked = true;
-        newIndex = 0;
-    }
 }
