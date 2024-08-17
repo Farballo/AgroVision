07f5c012c0cfc7f7c3c538583d760418c26be2b4
Facundo Arballo <farballo@fi.uba.ar>
2024-08-17 19:46:24 -0300

    Cambios pag Inicio

--- scripts.js
+++ scripts.js
@@ -33,32 +33,31 @@ function initializeImageObserver() {
     });
 }
 
-window.onscroll = function() {stickyHeader()};
+window.onscroll = function() { stickyHeader(); };
 
 var header = document.querySelector("header");
 var sticky = header.offsetTop + 2000; // Ajusta el valor para el punto donde el header debe dejar de ser fijo
 
 function stickyHeader() {
-    if (window.pageYOffset > sticky) {
-        header.classList.remove("sticky");
-        header.style.position = "absolute";
-        header.style.top = sticky + "px"; // Mantén el header en su posición
+    var width = window.innerWidth; // Recalcular el ancho de la pantalla en cada scroll
+
+    if (width > 1156) {
+        if (window.pageYOffset > sticky) {
+            header.classList.remove("sticky");
+            header.style.position = "absolute";
+            header.style.top = sticky + "px"; // Mantén el header en su posición fija
+        } else {
+            header.classList.add("sticky");
+            header.style.position = "fixed";
+            header.style.top = "0";
+        }
     } else {
-        header.classList.add("sticky");
-        header.style.position = "fixed";
-        header.style.top = "0";
+        // Para pantallas pequeñas, quitar la clase sticky y resetear el estilo
+        header.classList.remove("sticky");
+        header.style.position = "";
+        header.style.top = "";
     }
 }
-
-/* 
-else {
-    setTimeout(() => {
-        if (!entry.isIntersecting) {
-            entry.target.classList.remove("appear");
-        }
-    }, 5000);
-}
-*/
 
 function toggleMenu() {
     var navMenu = document.getElementById('nav-menu');
